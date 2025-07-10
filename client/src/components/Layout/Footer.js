a6ded61d59d5f7b240082a0055e650c4e6616c4f
Miruna-Miru <mirmirunalini@gmail.com>
2025-07-10 14:28:30 +0530

    First commit

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'; 
+
+const Footer = () => {
+    return (
+        <div className='bg-dark text-light p-4'>
+            <h6 className="text-center animated-text">All rights reserved &copy; FrameworkX</h6>
+        </div>
+    );
+}
+
+export default Footer;
